cef450e4837c4eca5176b7177c28d8d9ea55d045
GauravChandwani12 <chandwanigaurav35@gmail.com>
2024-08-17 20:05:17 +0530

    loggin and exception

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,2 @@
+## Step 1
+## data ingestion -> reading the data from database 
---
+++ src/components/data_transformation.py
@@ -0,0 +1,2 @@
+## Step 2 After data ingestion 
+## transforming the data from categorical feature to numerical feature and etc
